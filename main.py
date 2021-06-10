8ac8ace5037a37a19a7b2b154c190a2da6592119
HM <hieumdd@gmail.com>
2021-06-10 16:13:09 +0700

    Fix bug in broadcaster

--- main.py
+++ main.py
@@ -12,13 +12,14 @@ def main(request):
     message = request_json["message"]
     data_bytes = message["data"]
     data = json.loads(base64.b64decode(data_bytes).decode("utf-8"))
+    print(data)
 
     if "broadcast" in data:
         publisher = pubsub_v1.PublisherClient()
         topic_path = publisher.topic_path(
             os.getenv("PROJECT_ID"), os.getenv("TOPIC_ID")
         )
-        tables = [f"configs/{i}" for i in os.listdir("configs")]
+        tables = [i.replace('.json', '') for i in os.listdir("configs")]
         for table in tables:
             message_json = json.dumps({"table": table})
             message_bytes = message_json.encode("utf-8")
